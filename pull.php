a13fa2c43cb8f090fbe7ceb51f68f26a1bc5d55f
skarena20 <81649582+skarena20@users.noreply.github.com>
2021-03-30 15:23:29 -0500

    actualizando el primer archivo .php

---
+++ pull.php
@@ -0,0 +1,11 @@
+<?php
+    $persona = [
+        "nombre" => "Karina",
+        "apellido"=> "Pumacayo",
+        "edad" => 5
+        ];
+    
+        foreach ($persona as $valor) {
+          echo "$valor <br>";
+          }
+?>
